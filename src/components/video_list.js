7b8ddc84a2e69baf16975d41fe0b3329ab438fad
Jocelyn Belden <jocebelden@gmail.com>
2018-05-12 11:11:49 -1000

    change selected video

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,11 @@ import VideoListItem from "./video_list_item";
 
 const VideoList = props => {
   const videoItems = props.videos.map(video => (
-    <VideoListItem key={video.etag} video={video} />
+    <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video}
+    />
   ));
 
   return <ul className="col-md-4 list-group">{videoItems}</ul>;
