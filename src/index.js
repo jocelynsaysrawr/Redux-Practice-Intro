31cbe51cead9c6e181f09ab41b129ed1e386e25d
Jocelyn Belden <jocebelden@gmail.com>
2018-05-12 10:58:49 -1000

    load a single video detail

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyDTeURN5NfpGwwTGo03qALDZI6Gy_f_tHA";
 
-// Create a new component. This component should produce some HTML
-const App = () => <div>Hi!</div>;
-// Take this component's generated HTML and put it on the page (in the DOM)
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector(".container"));
